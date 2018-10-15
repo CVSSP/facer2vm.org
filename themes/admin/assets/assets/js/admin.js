f23556763e9a2ac5bbcbd7c2d534de00e9cfea65
Paul Koppen <p.koppen@surrey.ac.uk>
2018-10-15 21:31:28 +0100

    Start admin development.

---
+++ themes/admin/assets/assets/js/admin.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    var htmlClass = document.querySelector('html').classList
+    
+    var listInputs = document.querySelectorAll('input[type="text" & multiple]')
+    listInputs.forEach(function(elm) {
+        
+    })
+})
