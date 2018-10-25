9327a06e90d5a1e1341b552d798f995471d96ffe
Paul Koppen <gitlab@paulkoppen.com>
2018-10-25 11:02:31 +0100

    Stab one at embedding JSON-LD. Abandon admin theme.

--- themes/admin/assets/assets/js/admin.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    var htmlClass = document.querySelector('html').classList
-    
-    var listInputs = document.querySelectorAll('input[type="text" & multiple]')
-    listInputs.forEach(function(elm) {
-        
-    })
-})
